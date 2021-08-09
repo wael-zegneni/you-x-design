a7c572276eeef5532cf2541205904c1b88f7f787
yassine <yessine.hachfi@gmail.com>
2021-08-09 13:38:24 +0200

    secure db

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express()
 const port = 3000
 const connectDB = require('./helpers/connect')
+require('dotenv').config()
 app.get('/',(req,res)=>{
     res.send('<h1>you x design</h1>')
 })
@@ -10,7 +11,7 @@ app.get('/',(req,res)=>{
 
 const start = async() => {
     try {
-        await connectDB()
+        await connectDB(process.env.MONGO_URI)
         app.listen(port,()=>{
             console.log(`server is listening on port ${port}`)
         })
