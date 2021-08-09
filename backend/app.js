afb14789bb9e29f3316a633bf7256d0967ba9d6f
yassine <yessine.hachfi@gmail.com>
2021-08-09 12:57:21 +0200

    "database"

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,22 @@
-require('./db/connect')
+
 const express = require('express');
 const app = express()
 const port = 3000
-
+const connectDB = require('./db/connect')
 app.get('/',(req,res)=>{
     res.send('<h1>you x design</h1>')
 })
 
-app.listen(port,()=>{
-    console.log(`server is listening on port ${port}`)
-})
+
+const start = async() => {
+    try {
+        await connectDB()
+        app.listen(port,()=>{
+            console.log(`server is listening on port ${port}`)
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+start()
