59169d48f82aa253390fb884c693163822849642
yassine <yessine.hachfi@gmail.com>
2021-08-09 13:37:07 +0200

    secure db

--- backend/helpers/connect.js
+++ backend/helpers/connect.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const connectionString ="mongodb+srv://yassine:yassine123.@nodeexpressprojects.6njoh.mongodb.net/you-x-design?retryWrites=true&w=majority"
 const connectDB=(url)=>{
-    mongoose.connect(connectionString,{useNewUrlParser:true,useCreateIndex:true,useFindAndModify:false,useUnifiedTopology:true})
+    mongoose.connect(url,{useNewUrlParser:true,useCreateIndex:true,useFindAndModify:false,useUnifiedTopology:true})
 }
 module.exports= connectDB
-//.then(()=>console.log("Connected to DB ...")).catch((err)=>console.log(err))
