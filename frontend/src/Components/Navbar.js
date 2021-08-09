98dd77c76ecf2a428ba17fefb4be056ce06e600c
wael zegneni <wael-msk2@live.fr>
2021-08-09 11:03:47 +0100

    "front commit"

---
+++ frontend/src/Components/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import logo from '../Assets/Images/logo.jpg'
+import searchLogo from '../Assets/Images/searchLogo.jpg'
+import ReactLanguageSelect from 'react-languages-select';
+import 'react-languages-select/css/react-languages-select.css';
+
+
+import './style.css'
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <img src={logo} alt="youXdesign" />
+            <div className="searchBar">
+                <input type='text' />
+                <img src={searchLogo} alt="searchLogo" />
+            </div>
+            <ReactLanguageSelect className="langPicker" defaultLanguage="en" languages={["en", "fr", "ar"]} customLabels={{"en": "EN", "fr": "FR", "ar": "AR"}} />
+            <h1>Join our community</h1>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ frontend/src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../Components/Navbar'
+import './home.css'
+
+const Home = () => {
+    return (
+        <div className="body">
+            <Navbar/>
+        </div>
+    )
+}
+
+export default Home
