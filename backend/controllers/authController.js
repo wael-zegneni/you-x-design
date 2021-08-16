9f5891fbf436bf377be1f09a5367dd61236df96e
yassine <yessine.hachfi@gmail.com>
2021-08-16 15:32:15 +0200

    cleaner auth

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,19 +3,21 @@ const bcrypt = require ('bcrypt')
 const jwt = require('jsonwebtoken');
 const passport = require("passport");
 
-const signin = (req, res, next) => {
+const signin = function (req, res, next) {
     passport.authenticate('local', {session: false}, (err, user, info) => {
-        /*if (err || !user) {
+        
+        if (err || !user) {
             return res.status(400).json({
                 message: 'Something is not right',
                 user   : user });
            
-        }*/
+        }
        req.login(user, {session: false}, (error) => {
            if (error) {
                res.send(err);
            }
-           var token = jwt.sign(user, 'your_jwt_secret');
+           var token = jwt.sign(user.toJSON(), 'your_jwt_secret');
+           console.log(token)
            return res.json({user, token});
         });
     })(req, res);
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,28 +3,10 @@ const router = express.Router();
 const jwt = require('jsonwebtoken');
 const passport = require("passport");
 const {
-    sigin,
+    signin,
     register
 } = require ('../controllers/authController')
 
-router.post('/login', function (req, res, next) {
-    passport.authenticate('local', {session: false}, (err, user, info) => {
-        
-        if (err || !user) {
-            return res.status(400).json({
-                message: 'Something is not right',
-                user   : user });
-           
-        }
-       req.login(user, {session: false}, (error) => {
-           if (error) {
-               res.send(err);
-           }
-           var token = jwt.sign(user.toJSON(), 'your_jwt_secret');
-           console.log(token)
-           return res.json({user, token});
-        });
-    })(req, res);
-});
+router.route('/login').post(signin);
 router.route('/register').post(register);
 module.exports = router
