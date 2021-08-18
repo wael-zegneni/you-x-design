90f2a20ad74b588d7de3bd9dfe57f83741088983
yassine <yessine.hachfi@gmail.com>
2021-08-18 14:55:05 +0200

    test

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from '../../Components/navbar/Navbar'
+import CourseCard from '../../Components/course-card/CourseCard'
 import './home.css'
 
 const Home = () => {
     return (
         <div className="body">
             <Navbar/>
+            <CourseCard/>
+          
         </div>
+        
     )
 }
 
