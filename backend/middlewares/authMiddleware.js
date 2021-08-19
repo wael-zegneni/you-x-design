3e1378c3a70795544375554577f046e2dece3828
yassine <yessine.hachfi@gmail.com>
2021-08-19 12:00:18 +0200

    secret key

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,13 +1,16 @@
   const jwt = require("jsonwebtoken");
+  require('dotenv').config()
 
 module.exports = (req, res, next) => {
   let token = req.header("Authorization");
+  let secretKey = process.env.SECRET_KEY
+
 
   if (!token) {
     return res.status(401).json({ message: "You are not authorized!" });
   }
 
-  jwt.verify(token.split("Bearer ")[1], "your_jwt_secret", (err, payload) => {
+  jwt.verify(token.split("Bearer ")[1], secretKey, (err, payload) => {
     if (err) {
       throw err;
     }
