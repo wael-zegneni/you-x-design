a43e52a38ac18d3301b51b367ff45f13f6b9395c
yassine <yessine.hachfi@gmail.com>
2021-08-26 10:43:31 +0200

    instructor pics

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -4,7 +4,6 @@ const updateUser = async (req,res) => {
     try { 
     User.findByIdAndUpdate(req.body.id,{
         userName: req.body.name,
-        email: req.body.email,
         phone: req.body.phone,
         role: req.body.role,
         age : req.body.age,
@@ -40,43 +39,6 @@ const getInstructors = async (req,res) =>{
     console.log(instructors)
     res.send(instructors)
 }
-// let user =  await User.findOne({ email: req.body.RegisterEmail });
-//     if (user) {
-//         return res.status(400).json({errors :[{msg: "user already exists !"}]});
-//     } else {
-//         // Insert the new user if they do not exist yet
-//        try {
-//         const hashedPassword = await bcrypt.hash(req.body.RegisterPassword,10)
-//         console.log(hashedPassword)
-//             user = new User({
-//                 userName: req.body.name,
-//                 email: req.body.RegisterEmail,
-//                 phone: req.body.phone,
-//                 password: hashedPassword,
-//                 role: req.body.role,
-//                 age : req.body.age,
-//                 residence : req.body.residence,
-//                 bio : req.body.bio,
-//             });
-//             await user.save();
-//             var token = jwt.sign(user.toJSON(), process.env.SECRET_KEY);
-//             res.json({user, token});
-//        } catch (error) {
-//         res.status(400).send(error)
-//         }
-//        }
-    
-//     }
-// User.findByIdAndUpdate(user_id, { name: 'Gourav' },
-//                             function (err, docs) {
-//     if (err){
-//         console.log(err)
-//     }
-//     else{
-//         console.log("Updated User : ", docs);
-//     }
-// });
-
 
 module.exports = {
     updateUser,
