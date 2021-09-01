3b164ceec334064b83f8c64b21f4f809188d68a3
wael zegneni <wael-msk2@live.fr>
2021-09-01 14:40:40 +0100

    Pagination: 70%

---
+++ frontend/src/Components/pagination/Pagination.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Box, Center, Heading, Text, Stack, Avatar, useColorModeValue, Flex } from '@chakra-ui/react';
+
+const Pagination = () => {
+    return (
+        <Box bg="#F7F9FB">
+            hey
+        </Box>
+    )
+}
+
+export default Pagination
